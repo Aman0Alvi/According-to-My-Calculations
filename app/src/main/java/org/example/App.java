23ffc9377d2bab93c4b528adbc7b7dfb5ab73fa0
Aman0Alvi <aman0alvi@gmail.com>
2025-09-28 23:26:26 -0500

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App {
     System.out.println(calculator.evaluate("3 + 6 * 5")); // 33
     System.out.println(calculator.evaluate("4 * (2 + 3)")); // 20
     System.out.println(calculator.evaluate("(7 + 9) / 8")); // 2
-    System.out.println(calculator.evaluate("-3 + 5 * 2")); // 7 (shows unary minus works)
+    System.out.println(calculator.evaluate("-3 + 5 * 2")); // 7 (unary minus)
   }
 }
